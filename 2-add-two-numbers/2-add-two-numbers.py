5eddb988689ff2482c8a755b7e91ba864e85fd04
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-08-17 16:02:38 -0700

    Time: 72 ms (90.85%), Space: 13.9 MB (86.99%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -0,0 +1,38 @@
+class Solution:
+    def addTwoNumbers(self, l1, l2 ,c = 0):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        
+        dummy = ListNode()
+        cur = dummy
+        
+        carry = 0
+        
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            
+            #compute the new digit
+            val = v1 + v2 + carry
+            
+            carry = val//10
+            val =  val%10
+            cur.next = ListNode(val)
+            
+            #update pointers
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        return dummy.next
+            
+            
+    
+    
+    
+    
+    
+
