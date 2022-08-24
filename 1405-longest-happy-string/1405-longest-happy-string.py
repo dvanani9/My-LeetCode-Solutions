1fe30af648d0007a2b6027e2551f6bfae5dd70d7
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-08-24 16:43:27 -0700

    Time: 59 ms (20.62%), Space: 14 MB (44.73%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.py
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -1,19 +1,23 @@
 class Solution:
-    def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        counts = {"a": a, "b": b, "c": c}
-        happy = ""
-        
-        while True:
-            max_char, max_count = None, 0
-            
-            for char, count in counts.items():
-                if count > max_count and \
-                not (len(happy) > 1 and happy[-1] == char and happy[-2] == char):
-                    max_char = char
-                    max_count = count
-            
-            if max_char is None:
-                return happy
-            else:
-                happy += max_char
-                counts[max_char] -= 1
+	def longestDiverseString(self, a: int, b: int, c: int) -> str:
+		pq = []
+		if a > 0: heapq.heappush(pq,(-a,'a'))        
+		if b > 0: heapq.heappush(pq,(-b,'b'))        
+		if c > 0: heapq.heappush(pq,(-c,'c'))
+
+		ans = ''
+		while pq:
+			c, ch = heapq.heappop(pq)
+			if len(ans)>1 and ans[-1] == ans[-2] == ch:
+				if not pq: break
+                    
+				c2, ch2 = heapq.heappop(pq)
+				ans += ch2
+				c2 += 1
+				if c2: heapq.heappush(pq,(c2,ch2))
+			else:
+				ans += ch
+				c += 1
+			if c: heapq.heappush(pq,(c,ch))
+
+		return ans
