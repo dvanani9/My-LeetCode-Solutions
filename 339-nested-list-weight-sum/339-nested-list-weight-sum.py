13178784add6a7ddae6a6080862427dba546b6e3
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-08-24 18:32:54 -0700

    Time: 56 ms (30.65%), Space: 14.2 MB (48.85%) - LeetHub

---
+++ 339-nested-list-weight-sum/339-nested-list-weight-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger], depth = 1) -> int:
+        
+        temp_sum=0
+        
+        for nest in nestedList:
+            if nest.isInteger():
+                temp_sum += depth * nest.getInteger()
+            else:
+                temp_sum += self.depthSum(nest.getList(),depth+1)
+                
+        return temp_sum
+    
+    
