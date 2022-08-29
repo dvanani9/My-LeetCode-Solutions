44068925e472e6f2cd3503c6507359327576e73f
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-08-29 13:47:46 -0700

    Time: 318 ms (29.99%), Space: 17 MB (49.75%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.py
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.py
@@ -1,5 +1,13 @@
 class Solution(object):
-    def wiggleSort(self, nums):
-        for i, num in enumerate(sorted(nums, reverse=True)):
-            if i < len(nums) // 2:  nums[2*i+1] = num
-            else:   nums[len(nums)%2-2*(len(nums)-i)] = num
+    def wiggleSort(self, nums: List[int]) -> None:
+		#to make it work as a min-heap
+        heap = [-i for i in nums]
+        heapify(heap)
+		
+		#putting largest element into odd index
+        for i in range(1,len(nums),2):
+            nums[i] = -heappop(heap)
+		
+		#putting remaining element into even index
+        for i in range(0, len(nums), 2):
+            nums[i] = -heappop(heap)
