aefcadb2d317e2e5c3fbf97611b0170dd202aa50
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-08-29 23:12:50 -0700

    Time: 86 ms (94.47%), Space: 14.1 MB (95.44%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -1,9 +1,13 @@
 class Solution(object):
     def firstUniqChar(self, s):
-        hset = collections.Counter(s);
+        
+        hset = collections.Counter(s)
         # Traverse the string from the beginning...
         for idx in range(len(s)):
             # If the count is equal to 1, it is the first distinct character in the list.
             if hset[s[idx]] == 1:
                 return idx
         return -1       # If no character appeared exactly once...
+    
+    
+    
