8007bdb1f20636ac55e9919b41875d09ab783a4f
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-08-30 20:46:13 -0700

    Time: 66 ms (96.44%), Space: 17.7 MB (47.88%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.py
+++ 243-shortest-word-distance/243-shortest-word-distance.py
@@ -1,14 +1,20 @@
 class Solution:
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
-        indice1 = []
-        indice2 = []
-        min_distance = 3*10**4 + 1
-        for i, word in enumerate(wordsDict):
-            if word == word1:
-                indice1.append(i)
-            elif word == word2:
-                indice2.append(i)
-        for i in indice1:
-            for j in indice2:
-                min_distance = min(min_distance, abs(i-j))
-        return min_distance 
+        a,b=word1,word2
+        counta,countb=0,0
+        finala,finalb=math.inf,math.inf
+        flg1,flg2=False,False
+        for i in range(len(wordsDict)):
+            if wordsDict[i]==a:
+                counta=0
+                flg1=True
+                if flg2:
+                    finalb=min(countb,finalb)
+            elif wordsDict[i]==b:
+                countb=0
+                flg2=True
+                if flg1:
+                    finala=min(counta,finala)
+            counta+=1
+            countb+=1
+        return min(finala,finalb)
