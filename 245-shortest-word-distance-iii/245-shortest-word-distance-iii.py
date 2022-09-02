5a6c2032dc6fb709ea41a74d9cc2bdfb6f80fa1c
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-09-01 22:43:54 -0700

    Time: 1100 ms (60.03%), Space: 37.1 MB (25.78%) - LeetHub

---
+++ 245-shortest-word-distance-iii/245-shortest-word-distance-iii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shortestWordDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        p1 = p2 = -1
+        n = len(wordsDict)
+        ans = n
+        same = (word1 == word2)
+        
+        for i in range(n):
+            if wordsDict[i] == word1:
+                if same:
+                    # Store the previous index of word1
+                    p2 = p1
+                p1 = i
+            elif wordsDict[i] == word2:
+                # The 2nd condition will only get hit if word1 != word2
+                p2 = i
+                
+            if p1 >= 0 and p2 >= 0:
+                ans = min(ans, abs(p1 - p2))
+        
+        return ans
+    
+        
