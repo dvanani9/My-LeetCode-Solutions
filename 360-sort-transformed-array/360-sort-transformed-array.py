4d21710a0d51a9880a4570ce4b6e24423f42805f
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-09-10 18:25:18 -0700

    Time: 85 ms (22.54%), Space: 13.9 MB (92.84%) - LeetHub

---
+++ 360-sort-transformed-array/360-sort-transformed-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
+        
+        ans = []
+        
+        for i in range(len(nums)):
+            res = (a*(nums[i])**2) + (b*nums[i]) + c
+            ans.append(res)
+
+        return sorted(ans)
