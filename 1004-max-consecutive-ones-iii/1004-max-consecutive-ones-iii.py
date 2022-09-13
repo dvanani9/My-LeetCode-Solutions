738e8ea0e73668796a8259461625940cfd3e954d
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-09-12 22:30:33 -0700

    Time: 1718 ms (5.01%), Space: 14.6 MB (61.82%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -3,15 +3,26 @@ class Solution:
         j = 0
         d = {0:0}
         res = float('-inf')
+        
         for i in range(len(nums)):
             if nums[i] == 0:
                 d[0] += 1
+                
             if d[0] > k:
                 res = max(res, i-j)
+                
             while d[0] > k:
                 if nums[j] == 0:
                     d[0] -= 1
                 j += 1
+                
         return max(res, i-j+1)
+    
+    
+
+    
+    
+    
+    
     
     
