6f113b870745817326f13593c971e86842b59be6
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-09-13 18:39:58 -0700

    Time: 54 ms (46.61%), Space: 14 MB (59.27%) - LeetHub

---
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -0,0 +1,16 @@
+class HitCounter:
+    def __init__(self):
+        self.hitCounter = deque()
+        self.total = 0
+
+    def hit(self, timestamp: int) -> None:
+        if self.hitCounter and self.hitCounter[-1][0] == timestamp:
+            self.hitCounter[-1][1] += 1
+        else:
+            self.hitCounter.append([timestamp, 1])
+        self.total += 1
+        
+    def getHits(self, timestamp: int) -> int:
+        while self.hitCounter and self.hitCounter[0][0] <= timestamp - 300:
+            self.total -= self.hitCounter.popleft()[1]
+        return self.total
