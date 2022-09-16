2838e066e431b17550a7d0b96526b945b6c11e8c
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-09-15 22:43:29 -0700

    Time: 145 ms (25.83%), Space: 14.9 MB (16.93%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.py
+++ 867-transpose-matrix/867-transpose-matrix.py
@@ -2,8 +2,10 @@ class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
         rows=len(matrix)
         cols=len(matrix[0])
+        
         # create a 2D array for result with initial None values
         res=[[None]*rows for k in range(cols)]
+        
         # with every iteration update with transpose values in result array
         for i in range(rows):
             for j in range(cols):
