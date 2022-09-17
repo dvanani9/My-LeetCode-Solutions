4552a93ce86a17b52c4cecbd87f5df17d8afc226
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-09-17 14:23:37 -0700

    Time: 67 ms (71.18%), Space: 15.5 MB (29.96%) - LeetHub

---
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestRepeatingSubstring(self, s: str) -> int:
+        
+        def dfs(n):
+            subs = s[n:]
+            
+            if len(subs) == 1:
+                return 0
+            elif len(subs) == 2:
+                return int(subs[0] == subs[-1])
+            else:
+                prev = dfs(n+1)                    
+                check_str = subs[:prev+1]
+                rest_str = subs[1:]
+                if check_str in rest_str:
+                    prev = prev+1
+
+                return prev
+        return  dfs(0)
