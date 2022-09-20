7068d023bc8c6ae36c5cbcfcc2a87d82263d4562
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-09-19 19:19:06 -0700

    Time: 73 ms (17.79%), Space: 13.9 MB (32.55%) - LeetHub

--- 1864-minimum-number-of-swaps-to-make-the-binary-string-alternating/1864-minimum-number-of-swaps-to-make-the-binary-string-alternating.py
+++ 1864-minimum-number-of-swaps-to-make-the-binary-string-alternating/1864-minimum-number-of-swaps-to-make-the-binary-string-alternating.py
@@ -1,19 +1,33 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
-        zeros, ones = 0, 0
-        for c in s:
-            if c == "0":
-                zeros += 1
+        
+        ans = n = len(s)
+        zero, one = 0, 0
+        
+        for c in s:                                    # count number of 0 & 1s
+            if c == '0':
+                zero += 1
             else:
-                ones += 1
-        
-        if abs(zeros - ones) > 1:
-            return -1
-        
-        first = "1" if ones > zeros else "0"
-        invalid = 0
-        for c in s[::2]:
-            if c != first:
-                invalid += 1
+                one += 1
                 
-        return invalid if ones != zeros else min(invalid, (len(s)//2) - invalid)
+        if abs(zero - one) > 1:
+            return -1              # not possible when cnt differ over 1  
+        
+        if zero >= one:                                # '010101....' 
+            s1 = '01' * (n // 2)                       # when zero == one
+            s1 += '0' if n % 2 else ''                 # when zero > one 
+            cnt = sum(c1 != c for c1, c in zip(s1, s))
+            ans = cnt // 2
+            
+        if zero <= one:                                # '101010....'
+            s2 = '10' * (n // 2)                       # when zero == one
+            s2 += '1' if n % 2 else ''                 # when one > zero 
+            cnt = sum(c2 != c for c2, c in zip(s2, s))
+            ans = min(ans, cnt // 2)
+        return ans
+    
+    
+    
+    
+    
+    
