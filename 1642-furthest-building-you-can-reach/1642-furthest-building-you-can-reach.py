d035b0ba3f628a5b55efd0a41cfaa557859251cd
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-09-21 18:03:56 -0700

    Time: 1366 ms (23.99%), Space: 28.7 MB (5.96%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,35 +1,23 @@
+from collections import defaultdict
+
 class Solution:
-    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        res = [0]
-        for i in range(1,len(heights)):
-            p = heights[i]-heights[i-1]
-            if p>0:
-                res.append(p)
-            else:
-                res.append(0)
-        lo = 1
-        hi = len(res)-1
-        ans = 0
-        while(lo<=hi):
-            mid = (lo+hi)//2
-            def check(mid,bricks,ladder):
-                p = res[:mid+1]
-                p.sort()
-                for i in range(len(p)):
-                    if p[i]<=bricks:
-                        bricks-=p[i]
-                    else:
-                        if ladder>0:
-                            ladder-=1
-                        else:
-                            return False
-                return True
-            if check(mid,bricks,ladders):
-                lo = mid + 1
-                ans = mid
-            else:
-                hi = mid - 1
-        return ans
-    
-    
+    def furthestBuilding(self, h: List[int], bricks: int, lad: int) -> int:
         
+        l = [] #heap
+        heapq.heapify(l)
+        c=0
+        n =len(h)
+        
+        for i in range(1,n):
+            dif =h[i]-h[i-1]
+            if dif>0:
+                heapq.heappush(l,dif)
+                c+=1 
+                if c>lad:
+                    bricks-=heapq.heappop(l)
+                    c-=1
+                    if bricks<0:
+                        return i -1
+        return n-1
+    
+    
