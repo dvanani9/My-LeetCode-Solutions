bcd1bd38e5517a3de8c6b625ac71132879722853
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-09-23 10:42:26 -0700

    Time: 1733 ms (8.94%), Space: 29.4 MB (20.16%) - LeetHub

--- 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.py
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def minimalKSum(self, nums: List[int], k: int) -> int:
+        
         count=0
         n=sorted(list(set(nums)))
         for i in range(len(n)):
@@ -7,4 +8,10 @@ class Solution:
                 count+=n[i]
                 k+=1
         return k*(k+1)//2 - count
+    
+    
+    
+        
+        
+        
         
