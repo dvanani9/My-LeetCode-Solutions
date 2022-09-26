a26a021cfb24b51590bdd58d06ee94698f0314bb
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-09-26 02:18:07 -0700

    Time: 390 ms (19.82%), Space: 23.7 MB (89.84%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -1,23 +1,9 @@
 class Solution:
-    def singleNonDuplicate(self, arr: List[int]) -> int:
-        
-        start = 0
-        end = len(arr)-1
-
-        while (start < end):
-            mid = start + (end-start)//2
-
-            # handle when mid is odd
-            if (mid % 2 != 0):
-                if (arr[mid] != arr[mid+1]):
-                    start = mid+1
-                else:
-                    end = mid-1
-
-            # handle when mid is even
-            else:
-                if (arr[mid] == arr[mid+1]):
-                    start = mid+2
-                else:
-                    end = mid-1
-        return arr[start]
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l=0
+        h=len(nums)-2
+        while l<=h:
+            mid=(l+h)//2
+            if nums[mid]==nums[mid^1]: l=mid+1
+            else: h=mid-1
+        return nums[l]
