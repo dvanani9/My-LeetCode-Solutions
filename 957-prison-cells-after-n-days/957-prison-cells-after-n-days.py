95e900e7c22beada813fb1e730fc3d760e8e72bd
Dhruvit Vanani <104601481+dvanani9@users.noreply.github.com>
2022-09-27 12:33:34 -0700

    Time: 69 ms (44.67%), Space: 13.8 MB (78.57%) - LeetHub

---
+++ 957-prison-cells-after-n-days/957-prison-cells-after-n-days.py
@@ -0,0 +1,11 @@
+class Solution:
+    def prisonAfterNDays(self, cells: List[int], n: int) -> List[int]:
+        
+        n = n%14 if n%14!=0 else 14
+        
+        for _ in range(n):
+            new = [0]*8
+            for i in range(1,7):
+                if cells[i-1]==cells[i+1]: new[i] = 1
+            cells = new
+        return cells
